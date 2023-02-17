fb4e4725cbfd56fcd8a8c33aa917ae17bc494605
MohamedXAdel <122820327+MohamedXAdel@users.noreply.github.com>
2023-02-17 02:59:47 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main -Write a C program that prints exactly with function puts 
+ */
+int main (void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
