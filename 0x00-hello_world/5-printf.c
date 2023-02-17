21e2e936dd7b9cddf7c4f572daf4d7ddcf02d50d
MohamedXAdel <122820327+MohamedXAdel@users.noreply.github.com>
2023-02-17 03:29:57 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,followed by a new line.
+ * main -prints,followed by a new line.
  *
  * Return: Always 0 (Success)
  */
