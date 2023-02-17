9d92511608ac28b1ee6799e761ccb1a734ca7930
MohamedXAdel <122820327+MohamedXAdel@users.noreply.github.com>
2023-02-17 03:14:21 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * main - Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,followed by a new line.
  *
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
